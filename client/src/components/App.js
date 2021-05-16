59afdfb112eae5da58594cc1c2532632c0e19630
golonzovsky <alex.golonzovsky@gmail.com>
2021-05-16 18:19:02 +0200

    add scroll to active location list

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import moment from "moment";
 import LocationList from "./LocationList";
 import GithubRibbon from "./GithubRibbon";
 import Map from "./Map";
-//import dummyResp from "./dummyData.json"
+//import dummyResp from "./example_response.json"
 
 const {Header, Content} = Layout;
 const {Title, Text} = Typography;
@@ -26,12 +26,10 @@ function App() {
         }, 30 * 1000) // todo currently 30 sec poll, switch to SSE
         return () => clearInterval(interval)
     }, []);
-
 /*
     useEffect(() => {
         setData(dummyResp)
-    }, []);
-*/
+    }, []);*/
 
     return (
         <Layout style={{minHeight: "100vh"}}>
@@ -41,12 +39,12 @@ function App() {
             </Header>
             <Content>
                 <Row>
-                    <Col lg={{span: 6, offset: 0}} style={{minHeight: "100vh", padding: '30px'}}>
+                    <Col lg={{span: 6, offset: 0}} style={{height: "100vh", padding: '30px', overflow: 'auto'}}>
                         <Title level={3} style={{paddingTop: '50px'}}>Available slots for group N:</Title>
                         <LocationList locations={data.locations} onSelectLocation={setSelectedLocation}/>
                         <Text type="secondary" style={{paddingTop: '15px', display: 'block'}}>Last scan: {moment(data.last_refresh).fromNow()} (auto-refresh)</Text>
                     </Col>
-                    <Col lg={{span: 18, offset: 0}} style={{minHeight: "100vh"}}>
+                    <Col lg={{span: 18, offset: 0}} style={{height: "100vh"}}>
                         <Map locations={data.locations} selectedLocation={selectedLocation}/>
                     </Col>
                 </Row>
