219bf4fd1b265e5f89fee7e3d2a78344c6942532
golonzovsky <alex.golonzovsky@gmail.com>
2021-05-21 14:03:32 +0200

    add banner with info about issues with crawling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import {Col, Layout, Row, Typography} from 'antd';
+import {Col, Layout, Modal, Row, Typography} from 'antd';
 import axios from 'axios';
 import moment from "moment";
 import LocationList from "./LocationList";
@@ -26,10 +26,30 @@ function App() {
         }, 30 * 1000) // todo currently 30 sec poll, switch to SSE
         return () => clearInterval(interval)
     }, []);
-/*
+    /*
+        useEffect(() => {
+            setData(dummyResp)
+        }, []);*/
     useEffect(() => {
-        setData(dummyResp)
-    }, []);*/
+        Modal.info({
+            title: 'Data is outdated',
+            width: 800,
+            closable: false,
+            content: (
+                <div>
+                    <div style={{paddingTop: '30px'}}>
+                        <Text>It seems, <a href='https://zh.vacme.ch'>https://zh.vacme.ch</a> 'improved' their bot detection, which prevents our scraping.</Text>
+                    </div>
+                    <div style={{paddingTop: '30px'}}>
+                        <Text>Check other parser <a href='https://rimpfli.web.app'>https://rimpfli.web.app</a> - it seems they still manage to crawl.</Text>
+                    </div>
+                    <div style={{paddingTop: '30px'}}>
+                        <Text type='secondary'>We are working on a fix. Until its done - data will be stale/partial.</Text>
+                    </div>
+                </div>
+            ),
+        });
+    }, []);
 
     return (
         <Layout style={{minHeight: "100vh"}}>
@@ -42,7 +62,8 @@ function App() {
                     <Col lg={{span: 6, offset: 0}} style={{height: "100vh", padding: '30px', overflow: 'auto'}}>
                         <Title level={3} style={{paddingTop: '50px'}}>Available slots for group N:</Title>
                         <LocationList locations={data.locations} onSelectLocation={setSelectedLocation}/>
-                        <Text type="secondary" style={{paddingTop: '15px', display: 'block'}}>Last scan: {moment(data.last_refresh).fromNow()} (auto-refresh)</Text>
+                        <Text type="secondary" style={{paddingTop: '15px', display: 'block'}}>Last
+                            scan: {moment(data.last_refresh).fromNow()} (auto-refresh)</Text>
                     </Col>
                     <Col lg={{span: 18, offset: 0}} style={{height: "100vh"}}>
                         <Map locations={data.locations} selectedLocation={selectedLocation}/>
