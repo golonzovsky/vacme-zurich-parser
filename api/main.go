87d8ab570e82a555a134273d7789a5233288c28c
golonzovsky <alex.golonzovsky@gmail.com>
2021-12-26 20:20:49 +0100

    server timeouts, extract route configs

--- api/main.go
+++ api/main.go
@@ -13,28 +13,47 @@ import (
 	"time"
 )
 
+type server struct {
+	router *gin.Engine
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	s.router.ServeHTTP(w, req)
+}
+
+func (s *server) registerRoutes() {
+	s.router.GET("/health", handlers.Ok)
+	s.router.GET("/metrics", handlers.PrometheusMetrics())
+	//v2 := r.Group("/api/v2")
+	s.router.POST("/api/v2/log", handlers.Log) //todo rename to twillio callback and check hmac(sha1)
+	s.router.GET("/api/v2/locations", locations.NewFetcher().Handler)
+}
+
+func newServer() *server {
+	s := &server{gin.New()}
+	s.registerRoutes()
+	loggerIgnoreSysPath := gin.LoggerWithConfig(gin.LoggerConfig{
+		SkipPaths: []string{"/health", "/metrics"},
+	})
+	s.router.Use(loggerIgnoreSysPath, gin.Recovery())
+	return s
+}
+
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	initLogger()
 
-	r := gin.New()
-	r.Use(loggerIgnoreSysPaths(), gin.Recovery())
-
-	r.GET("/health", handlers.Ok)
-	r.GET("/metrics", handlers.PrometheusMetrics())
-
-	//v2 := r.Group("/api/v2")
-	r.POST("/api/v2/log", handlers.Log) //todo rename to twillio callback and check hmac(sha1)
-	r.GET("/api/v2/locations", locations.NewFetcher().Handler)
-
-	//r.Run("0.0.0.0:8000")
 	srv := &http.Server{
-		Addr:    ":8000",
-		Handler: r,
+		Addr:         ":8000",
+		Handler:      newServer(),
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
 	}
 	go func() {
+		log.Infof("Listening on %v", srv.Addr)
 		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
 			log.Printf("listen: %s\n", err)
 		}
@@ -54,12 +73,6 @@ func registerGracefulShutdown(ctx context.Context, stop context.CancelFunc, srv 
 		log.Fatal("Server forced to shutdown:", err)
 	}
 	log.Println("Server exiting")
-}
-
-func loggerIgnoreSysPaths() gin.HandlerFunc {
-	return gin.LoggerWithConfig(gin.LoggerConfig{
-		SkipPaths: []string{"/health", "/metrics"},
-	})
 }
 
 func initLogger() {
